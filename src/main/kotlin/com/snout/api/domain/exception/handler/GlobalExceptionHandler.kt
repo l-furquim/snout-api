c20a9499b089aa68c9fc0c1f0445c8d269cff27d
l-furquim <lucas.furquim@sptech.school>
2024-12-08 19:03:49 -0300

    add the global exception handler

--- src/main/kotlin/com/snout/api/domain/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/snout/api/domain/exception/handler/GlobalExceptionHandler.kt
@@ -5,22 +5,21 @@ import com.snout.api.domain.exception.body.GlobalExceptionBody
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
-class NoteExceptionHandler {
+class GlobalExceptionHandler {
 
-    @ExceptionHandler(NoContentNoteException::class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     fun handleNoContentNote(
-        exception: NoContentNoteException,
+        exception: Exception,
         request: HttpServletRequest
     ): GlobalExceptionBody {
         return GlobalExceptionBody(
             errorMessage = exception.message,
-            status = HttpStatus.UNAUTHORIZED.value(),
+            status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
             url = request.servletPath
         )
     }
